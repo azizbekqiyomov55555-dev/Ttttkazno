7eb057fbdbfd7be152f89ea685b58345480fec0b
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-22 18:17:30 +0500

    Update services.py

--- handlers/services.py
+++ handlers/services.py
@@ -1,4 +1,5 @@
-from aiogram import Router, F, types
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from database import db
